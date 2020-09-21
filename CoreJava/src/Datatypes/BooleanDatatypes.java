ba8b9adf5b7311c7817be310f5c8c3d7d593a28f
rmuthusami <raja_muthusami@yahoo.com>
2020-09-21 18:19:08 -0400

    adding class

---
+++ CoreJava/src/Datatypes/BooleanDatatypes.java
@@ -0,0 +1,4 @@
+package Datatypes;
+
+public class BooleanDatatypes {
+}
---
+++ CoreJava/src/Datatypes/StringBasics.java
@@ -0,0 +1,14 @@
+package Datatypes;
+
+public class StringBasics {
+    public static void main(String[] args) {
+        int age = 20;
+        int height = 6;
+
+        int finalValue = age + height;
+
+        System.out.println(finalValue);
+
+        String name = "Peter";
+    }
+}
